89bbd51122d08b288eab52f60dcb2368ff1a1657
Alexis <git@alexistats.com>
2020-12-04 18:54:46 -0500

    modified some comments

--- day4/aoc_day4.sql
+++ day4/aoc_day4.sql
@@ -1,8 +1,6 @@
 create table public.day4_input(
 input varchar
 );
--- For this problem I had to paste special the data into the CSV in order to show null rows, these will be useful 
---   to format the data into separated passports (I'm trying to do minimal manipulations outside of SQL)
 copy public.day4_input 
 from '/path/to/input.txt'
 delimiter ',';
@@ -56,7 +54,7 @@ where (count_fields = 8) or (count_fields = 7 and fields not LIKE '%cid:%');
 
 
 
--- part 2: I just have to add conditions to my where clause from part 1
+-- part 2: I have to extract the fields and add conditions to my where clause from part 1
 with 
 day4_input_2 as ( 
 select null as input
@@ -117,23 +115,3 @@ select count(*) from (
 	;
 
 
-
-
-
-
-
-
-
-
---step 2
-select count(*) from (
-	select char_length(fields) - char_length(replace(fields, ':', '')) as count_fields, * from (
-		select string_agg(input, ' ') as fields, passport 
-		from to_listagg
-		group by passport) as alias) as alias_2
-where
---part 1 conditions
-(count_fields = 8) or (count_fields = 7 and fields not LIKE '%cid:%')
---part 2 conditions
-and 
-;
